f8d0d3543b09407ac85b19a8b81a121bed3010f3
vladislemon <vladislemon@gmail.com>
2022-09-12 21:46:56 +0300

    Very basic Win32 window creation

--- src/window_windows.c
+++ src/window_windows.c
@@ -1,1 +1,126 @@
 #include "window_windows.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
+#define INVALID_WINDOW 0xFFFFFFFF
+#define MAX_WINDOW_COUNT 8
+
+typedef struct window_data_s {
+    HWND handle;
+    WNDCLASSEX class;
+    bool close_requested;
+} WindowData;
+
+HINSTANCE window_windows_module_handle = 0;
+Window window_windows_window_count = 0;
+WindowData *window_windows_windows_data = 0;
+
+Window window_windows_get_window_by_handle(HWND handle) {
+    for (u32 i = 0; i < window_windows_window_count; i++) {
+        if (window_windows_windows_data[i].handle == handle) {
+            return i;
+        }
+    }
+    return INVALID_WINDOW;
+}
+
+bool window_windows_handle_message(MSG *message, int status) {
+    if (status == 0) {
+        return false;
+    } else if (status == -1) {
+        printf("WIN32 Error: %lu\n", GetLastError());
+        return true;
+    } else {
+        TranslateMessage(message);
+        DispatchMessage(message);
+        return true;
+    }
+}
+
+void window_global_poll_events() {
+    MSG message;
+    while (window_windows_handle_message(&message, PeekMessage(&message, NULL, 0, 0, PM_REMOVE)));
+}
+
+void window_global_wait_events() {
+    MSG message;
+    window_windows_handle_message(&message, GetMessage(&message, NULL, 0, 0));
+    window_global_poll_events();
+}
+
+LRESULT CALLBACK window_windows_window_function(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_CLOSE: {
+            Window window = window_windows_get_window_by_handle(handle);
+            window_windows_windows_data[window].close_requested = true;
+            break;
+        }
+        case WM_PAINT: {
+            ValidateRect(handle, NULL);
+            break;
+        }
+        default: {
+            return DefWindowProc(handle, message, wParam, lParam);
+        }
+    }
+    return 0;
+}
+
+Window window_create(u16 width, u16 height, const char *title) {
+    if (window_windows_window_count == MAX_WINDOW_COUNT) {
+        return INVALID_WINDOW;
+    }
+    if (window_windows_window_count == 0) {
+        window_windows_module_handle = GetModuleHandle(NULL);
+        window_windows_windows_data = malloc(sizeof(WindowData) * MAX_WINDOW_COUNT);
+    }
+    WindowData *window_data = &window_windows_windows_data[window_windows_window_count];
+    window_data->class.cbSize = sizeof(WNDCLASSEX);
+    window_data->class.style = CS_HREDRAW | CS_VREDRAW;
+    window_data->class.lpfnWndProc = window_windows_window_function;
+    window_data->class.cbClsExtra = 0;
+    window_data->class.cbWndExtra = 0;
+    window_data->class.hInstance = window_windows_module_handle;
+    window_data->class.hIcon = NULL;
+    window_data->class.hCursor = LoadCursor(NULL, IDC_ARROW);
+    window_data->class.hbrBackground = (HBRUSH) GetStockObject(BLACK_BRUSH);
+    window_data->class.lpszMenuName = NULL;
+    window_data->class.lpszClassName = title;
+    window_data->class.hIconSm = NULL;
+    ATOM class_atom = RegisterClassEx(&window_data->class);
+    window_data->handle = CreateWindowEx(
+            0,
+            (LPCSTR) MAKELPARAM(class_atom, 0),
+            title,
+            WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            width, height,
+            NULL,
+            NULL,
+            window_windows_module_handle,
+            NULL
+    );
+    window_data->close_requested = false;
+    ShowWindow(window_data->handle, SW_SHOW);
+    return window_windows_window_count++;
+}
+
+void window_destroy(Window window) {
+    if (window >= window_windows_window_count) {
+        return;
+    }
+    DestroyWindow(window_windows_windows_data[window].handle);
+    window_windows_window_count--;
+    if (window_windows_window_count == 0) {
+        free(window_windows_windows_data);
+        window_windows_windows_data = 0;
+    }
+}
+
+bool window_is_close_requested(Window window) {
+    if (window >= window_windows_window_count) {
+        return false;
+    }
+    return window_windows_windows_data[window].close_requested;
+}
--- src/window_windows.h
+++ src/window_windows.h
@@ -1,5 +1,8 @@
 #ifndef CGFS_WINDOW_WINDOWS_H
 #define CGFS_WINDOW_WINDOWS_H
 
+#include "types.h"
+
+typedef u32 Window;
 
 #endif //CGFS_WINDOW_WINDOWS_H
